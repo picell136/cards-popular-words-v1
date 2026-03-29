cba6b86bdb50bc5d90dd5315beb44f4793034f13
Andrey <susluk93@mail.ru>
2026-03-29 23:21:32 +0300

    Added main.jsx

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/cards-popular-words-v1",
+    base: "/cards-popular-words-v1/",
+    server: {
+        host: '127.0.0.1'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+        loader: {
+            '.js': 'jsx',
+        },
+    },
+  },
 })
